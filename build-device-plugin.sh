7a62b956a3062459b1bd5db1e35757473422fc4f
Kieran Mansley <kieran@recoil.org>
2018-10-02 11:40:41 +0100

    Replace zvonkok with <username> in quay.io URLs

--- build-device-plugin.sh
+++ build-device-plugin.sh
@@ -1,8 +1,8 @@
 cd container
 
 docker build  -t sfc-dev-plugin  .
-docker tag sfc-dev-plugin quay.io/zvonkok/sfc-dev-plugin
-docker push quay.io/zvonkok/sfc-dev-plugin
+docker tag sfc-dev-plugin quay.io/<USERNAME>/sfc-dev-plugin
+docker push quay.io/<USERNAME>/sfc-dev-plugin
 
 
 cd ..
